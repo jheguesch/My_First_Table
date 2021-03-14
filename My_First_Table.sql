8b940d5dcadebb2327370e9c44ffd06ff2bb1438
João Heguedusch <ojoao6926@gmail.com>
2021-03-14 18:43:35 -0300

    Add files via upload

---
+++ My_First_Table.sql
@@ -0,0 +1,22 @@
+create table livraria
+(LIVRO varchar(100),
+AUTOR varchar(100),
+SEXO char(1),
+PAGINAS int(5),
+EDITORA varchar(30),
+VALOR float(10, 2),
+UF char(2),
+ANO int(4)
+);
+
+
+INSERT INTO LIVRARIA VALUES('Cavaleiro Real', 'Ana Claudia', 'F', 465, 'Atlas', 49.9, 'RJ', 2009);
+INSERT INTO LIVRARIA VALUES('SQL para leigos', 'João Nunes', 'M', 450, 'Addison', 98, 'SP', 2018);
+INSERT INTO LIVRARIA VALUES('Receitas Caseiras', 'Celia Tavares', 'F', 210, 'Atlas', 45, 'RJ', 2008);
+INSERT INTO LIVRARIA VALUES('Pessoas Efetivas', 'Eduardo Santos', 'M', 390, 'Beta', 78.99, 'RJ', 2018);
+INSERT INTO LIVRARIA VALUES('Hábitos Saudáveis', 'Heduardo Santos', 'M', 630, 'Beta', 150.98, 'RJ', 2019);
+INSERT INTO LIVRARIA VALUES('A Casa Marrom', 'Hermes Macedo', 'M', 250, 'Bubba', 60, 'MG', 2016);
+INSERT INTO LIVRARIA VALUES('Estacio Querido', 'Geraldo Francisco', 'M', 310, 'Insignia', 100, 'ES', 2015);
+INSERT INTO LIVRARIA VALUES('Pra Sempre Amigas', 'Leda Silva', 'F', 510, 'Insignia', 78.98, 'ES', 2011);
+INSERT INTO LIVRARIA VALUES('Copas Inesqueciveis', 'Marco Alcantara', 'M', 200, 'Larson', 130.98, 'RS', 2018);
+INSERT INTO LIVRARIA VALUES('O Poder da Mente', 'Clara Mafra', 'F', 120, 'Continental', 56.58, 'RS', 2017);  
